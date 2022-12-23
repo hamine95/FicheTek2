20ac24e58a83f43b68dbafb16bf9129279e3cbef
flyaway2 <h.amine.dz@hotmail.fr>
2022-12-23 20:31:54 +0000

    commit rest

---
+++ WpfApp1/TestWin.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    /// <summary>
+    ///     Interaction logic for TestWin.xaml
+    /// </summary>
+    public partial class TestWin : Window
+    {
+        public TestWin()
+        {
+            InitializeComponent();
+        }
+    }
+}
